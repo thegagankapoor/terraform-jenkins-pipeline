2a8662034ec277cab2e87525cef2d919bd9d49e8
thegagankapoor <gagankapoorr@gmail.com>
2025-07-03 15:36:54 +0000

    main.tf_1

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "name" {
-  ami = ami-0f918f7e67a3323f0
-  instance_type = t2.micro
-  key_name = "practice-key"
+  ami = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+
 }
